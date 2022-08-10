64af3eac30c48c9dd05b004271924bd898955aab
Ryan Stahr <ryan.j.stahr@gmail.com>
2022-08-09 20:12:50 -0500

    functional delete test written. someone tell me if I did it wrong lol

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import random
 import faker_commerce
 from faker import Faker
@@ -75,3 +76,21 @@ class ProductTests(APITestCase):
         response = self.client.get('/api/products')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), Product.objects.count())
+    
+    def test_delete_product(self):
+        """
+        ensure we can delete a product
+        
+        """
+        product = Product.objects.first()
+        
+        url = f'api/products/{product.id}'
+        
+        response = self.client.delete(url)
+        
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        
+        response = self.client.get(url)
+        
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        
